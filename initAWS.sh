35df5f098f7a74ec6391e7e5168f64e80b0e3825
Bosque77 <forestschwrtz@gmail.com>
2019-09-27 20:54:22 -0400

    Startup Files

---
+++ initAWS.sh
@@ -0,0 +1,15 @@
+virtualenv -p /usr/bin/python3 myvenv
+cd ./myvenv/bin
+pwd
+source activate
+echo "activated venv"
+cd ..
+cd ..
+pwd
+pip install django==2.1.1
+django-admin startproject ebdjango
+cd ebdjango
+mkdir .ebextensions
+pip freeze > requirements.txt
+cd ..
+mv django.config ./ebdjango/.ebextensions
